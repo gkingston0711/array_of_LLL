1e051037ff8c323cf336476dd5a108025137abf1
Joey Kingston <48415221+gkingston0711@users.noreply.github.com>
2020-03-07 15:22:43 -0800

    newest

--- Main.java
+++ Main.java
@@ -3,131 +3,191 @@ import java.util.Scanner;
 import java.io.*;
 
 
-
-public class Main {
-
-    public static int display_main_menu(){
-        int pick=-1;
-
-        input = new Scanner(System.in);
-
-        System.out.println("Welcome to your daily planner please choose one of the options\n"+
-                "1:Add item to planner\n"+
-                "2:display entire planner\n"+
-                "3:display one of your activities\n");
-
-        pick = input.nextInt();
-        return pick;
-    }
-
-    public static int display_add_menu(){
-
-        int new_pick=-1;
-
-        input = new Scanner(System.in);
-
-        System.out.println("Hello user, which one of the events would you like to add to your calendar?\n" +
-                "1:Dinner\n" +
-                "2:work\n" +
-                "3:fun\n");
-
-        new_pick=input.nextInt();
-        return new_pick;
-    }
-
-    public static int process_option(int option){
-        int pick=-1;
-
-        switch(option){
-            case 1:
-                pick=display_add_menu();
-            case 2:
-
-            case 3:
-
-            default:break;
-        }
-        return 0;
-    }
-
-
-
-
+public class Main{
     private static Scanner input;
-    private static int pick;
+    private static int option;
 
     public static void main(String[] args) {
 
-/*
-        int test=display_main_menu();
+        option=-1;
+        input = new Scanner(System.in);
+        list object=new list();
 
-        if(test==1) {
-            int test2 = process_option(test);
+        while(option!=0) {
+            System.out.println("Welcome to your daily planner please choose one of the options\n" +
+                    "1:Add item to planner\n" +
+                    "2:display entire planner\n" +
+                    "3:display one of your activities\n" +
+                    "4:change time for a dinner event\n"+
+                    "5:delete item in calendar\n" +
+                    "6:see if your calendar is empty\n"+
+                    "0:exit this program");
+            option = input.nextInt();
+
+
+            if(option==1) {
+
+                System.out.println("Which one of the events would you like to add to your calendar?\n" +
+                        "1:Dinner\n" +
+                        "2:work\n" +
+                        "3:fun\n");
+                option=input.nextInt();
+
+                    if(option==1) {
+
+                        int DAY;
+                        String GROUP;
+                        String TIME;
+                        String HOST;
+
+
+                        input = new Scanner(System.in);
+
+                        System.out.println("Please enter what day your event is");
+                        DAY = input.nextInt();
+                        input.nextLine();
+
+                        System.out.println("Please enter name event: ");
+                        GROUP = input.nextLine();
+
+                        System.out.println("Please enter time of event: ");
+                        TIME = input.nextLine();
+
+                        System.out.println("Please enter the hosts name: ");
+                        HOST = input.nextLine();
+
+                        dinner party = new dinner(DAY, GROUP, TIME, HOST);
+                        Event new_event = (Event) party;//upcasting;
+
+                        object.insert(party, option-1);
+                    }
+
+                    if(option==2){
+
+                        int DAY;
+                        String NAME;
+                        String TIME;
+                        int PRIORITY;
+
+
+                        input = new Scanner(System.in);
+
+                        System.out.println("Please enter what day your event is");
+                        DAY = input.nextInt();
+                        input.nextLine();
+
+                        System.out.println("Please enter Event title: ");
+                        NAME = input.nextLine();
+
+                        System.out.println("Please enter time of event: ");
+                        TIME = input.nextLine();
+
+                        System.out.println("Please enter your priority: ");
+                        PRIORITY = input.nextInt();
+
+                        work party = new work(DAY, NAME, TIME, PRIORITY);
+                        Event new_event = (Event) party;//upcasting;
+
+                        object.insert(party,option-1);
+                    }
+
+                    if(option==3){
+
+                        int DAY;
+                        String NAME;
+                        String TIME;
+                        String TYPE;
+
+
+                        input = new Scanner(System.in);
+
+                        System.out.println("Please enter what day your event is");
+                        DAY = input.nextInt();
+                        input.nextLine();
+
+                        System.out.println("Please enter Event title: ");
+                        NAME = input.nextLine();
+
+                        System.out.println("Please enter time of event: ");
+                        TIME = input.nextLine();
+
+                        System.out.println("Please enter the hosts name: ");
+                        TYPE = input.nextLine();
+
+                        fun party = new fun(DAY, NAME, TIME,TYPE);
+                        Event new_event = (Event) party;//upcasting;
+
+                        object.insert(party, option-1);
+                    }
+            }
+
+            if(option==2){
+
+             object.display();
+
+            }
+            if(option==3){
+
+                System.out.println("Which would you like to print?\n"+
+                        "1:Dinner\n"+
+                        "2:Work\n"+
+                        "3:Fun\n");
+
+                option = input.nextInt();;
+
+                object.display_specific(option-1);
+
+            }
+            if(option==0) {
+                System.out.println("Thank you for using this program");
+            }
+
+            if(option==4){
+                    String new_time;
+                    String old_name;
+
+                    System.out.println("what is the name of the event you want to change times of: ");
+                    old_name=input.nextLine();
+                    input.nextLine();
+
+                    System.out.println("what is the new time of the event: ");
+                    new_time=input.nextLine();
+
+                    object.change_dinner_time(new_time,old_name);
+
+                }
+            if(option==5){
+
+                String delete_name;
+                int type;
+
+                System.out.println("name of event you want to delete: ");
+                delete_name=input.nextLine();
+                input.nextLine();
+
+                System.out.println("type 0 for dinner, 1 for work, 2 for fun");
+                type=input.nextInt();
+
+
+                object.delete_special(delete_name,type);
+
+            }
+
+            if(option==6){
+
+                int EMPTY;
+
+                EMPTY=object.is_empty();
+
+                if(EMPTY==0){
+                    System.out.println("YOUR LIST IS EMPTY");
+
+                }
+                else if(EMPTY==1){
+                    System.out.println("YOUR LIST HAS EVENTS IN IT");
+                }
+            }
         }
-*/
-            list object=new list();
 
-
-        dinner party=new dinner(5,"me, jake, D","7 pm","jake");
-        Event new_event = (Event) party;//upcasting
-
-        dinner party1=new dinner(9,"annie, maurnet","6 pm","annie");
-        Event new_event1 = (Event) party1;//upcasting
-
-        dinner party2=new dinner(5,"me, jake, Daniel","7 pm","Zelda");
-        Event new_event2 = (Event) party2;//upcasting
-
-        dinner party3=new dinner(9,"annie, maurnet","6 pm","Shmee");
-        Event new_event3 = (Event) party3;//upcasting
-
-        dinner party4=new dinner(20,"zzzzzzzzzzzzzzzz","TEST TEST","NEEEEWWWW");
-        Event new_event4 =(Event) party4;//upcasting
-        object.insert(party,2);
-        object.insert(party1,2);
-        object.insert(party2,2);
-        object.insert(party3,2);
-        System.out.println("TEST ARRAY 0 IS BELOW**********************");
-        object.insert(party4,0);
-        object.insert(party1,0);
-        object.insert(party2,0);
-        object.insert(party3,0);
-
-        input = new Scanner(System.in);
-        System.out.println("Hello user, which one of the events would you like to add to your calendar?\n" +
-                "1:Dinner\n" + "2:work\n" + "3:fun\n");
-        pick = input.nextInt();
-
-        System.out.println("your pick is: "+pick);
-
-
-
-
-
-
-       /*
-        input = new Scanner(System.in);
-        System.out.print("please enter your full name: ");
-        this.name = input.nextLine();
-        System.out.print("please enter your age:");
-        this.age = input.nextInt();
-        input.nextLine();  //line cin.ignore up until a new line
-        System.out.print("please enter your wage");
-        this.wage = input.nextFloat();
-        input.nextLine();
-        */
-
-
-
-        //work thing=new work(20,"call","noon",10);
-
-        //fun b_day=new fun(1,"my birthday","4PM","birthday");
-
-        //b_day.display();
-
-        //thing.display();
-
-        //party.display();
-
-        object.display();
     }
 }
